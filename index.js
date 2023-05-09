59f6f1f0f6e6a05b9f021b9bd25dddb767c95166
Isaac Rutledge-Wilson <devpreneurpro@proton.me>
2023-05-08 18:24:15 -0700

    finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, partialName) {
+    return drivers.filter(driver => driver.startsWith(partialName));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  module.exports = { findMatching, fuzzyMatch, matchName };
+  
